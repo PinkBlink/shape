dd562eaa86ba84af08ef1aaff441220af0de2404
PinkBlink <m.pink.blink@gmail.com>
2024-06-24 16:41:11 +0200

    refactor;

--- src/main/java/org/triangle/entity/Triangle.java
+++ src/main/java/org/triangle/entity/Triangle.java
@@ -86,8 +86,12 @@ public class Triangle {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Triangle triangle = (Triangle) o;
         return a.equals(triangle.a) && b.equals(triangle.b) && c.equals(triangle.c);
     }
